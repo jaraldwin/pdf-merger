d997c27d7d3f06d23ab5b6b7af5ee72735ed587f
jaraldwin <56905645+jaraldwin@users.noreply.github.com>
2025-10-24 16:05:52 +0800

    add ocr

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
+  eslint: { ignoreDuringBuilds: true },
+  webpack(config) {
+    config.experiments = { asyncWebAssembly: true, layers: true };
+    return config;
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // apply globally
+        headers: [
+          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+        ],
+      },
+    ];
   },
 };
 
